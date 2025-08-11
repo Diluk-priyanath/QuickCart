1c844a6a11b4bd2e6fbd92b6327503fa0780c83f
Diluk-priyanathh <dilukpriyanath26@gmail.com>
2025-08-11 15:37:11 +0530

    save

--- config/inngest.js
+++ config/inngest.js
@@ -62,3 +62,32 @@ export const syncUserDeletion = inngest.createFunction(
     await User.findByIdAndDelete(id);
   }
 );
+
+//inngest function to create order in database
+export const createUserOrder = inngest.createFunction(
+  { 
+    id: "create-user-order",
+    batchEvents: {
+      maxSize: 25,
+      timeout: '5s'
+    },
+  },
+  { event: "order/created" },
+  async ({ event }) => {
+
+    const orders = events.map(async (event) => {
+      return {
+        userId: event.data.userId,
+        address: event.data.address,
+        items: event.data.items,
+        amount: event.data.amount,
+        date: event.data.date,
+        
+      };
+    });
+
+    await connectDB();
+    await Order.insertMany(orders);
+
+    return { success: true, processed: orders.length };
+  })
--- context/AppContext.jsx
+++ context/AppContext.jsx
@@ -1,8 +1,12 @@
 'use client'
 import { productsDummyData, userDummyData } from "@/assets/assets";
-import { useUser } from "@clerk/nextjs";
+import { useAuth, useUser } from "@clerk/nextjs";
 import { useRouter } from "next/navigation";
-import { createContext, useContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useState, } from "react";
+import toast from "react-hot-toast";
+import axios from "axios";
+
+
 
 export const AppContext = createContext();
 
@@ -16,6 +20,7 @@ export const AppContextProvider = (props) => {
     const router = useRouter()
 
     const { user } =useUser()
+    const { getToken } = useAuth()
 
     const [products, setProducts] = useState([])
     const [userData, setUserData] = useState(false)
@@ -23,11 +28,39 @@ export const AppContextProvider = (props) => {
     const [cartItems, setCartItems] = useState({})
 
     const fetchProductData = async () => {
-        setProducts(productsDummyData)
+        try {
+            const { data } = await axios.get('/api/product/list');
+            if (data.success) {
+                setProducts(data.products);
+            } else {
+                toast.error(data.message);
+            }
+        } catch (error) {
+            toast.error(error.message);
+        }
     }
 
     const fetchUserData = async () => {
-        setUserData(userDummyData)
+        try {
+            if (user.publicMetadata.role === "seller") {
+                setIsSeller(true)
+            }
+            
+            const token = await getToken()
+
+            const {data} = await axios.get('/api/user/data', {
+                headers: { Authorization: `Bearer ${token}` }
+            })
+
+            if (data.success) {
+                setUserData(data.user);
+                setCartItems(data.user.cartItems);
+            } else {
+                toast.error(data.message);
+            }
+        } catch (error) {
+            console.error(error.message);
+        }
     }
 
     const addToCart = async (itemId) => {
@@ -40,6 +73,17 @@ export const AppContextProvider = (props) => {
             cartData[itemId] = 1;
         }
         setCartItems(cartData);
+        
+        if (user) {
+            try {
+                const token = await getToken();
+                await axios.post('/api/cart/update', { cartData }, { headers: { Authorization: `Bearer ${token}` } });
+                toast.success("Item added to cart");
+
+            } catch (error) {
+                toast.error(error.message);
+            }
+        }
 
     }
 
@@ -51,8 +95,18 @@ export const AppContextProvider = (props) => {
         } else {
             cartData[itemId] = quantity;
         }
-        setCartItems(cartData)
 
+        setCartItems(cartData)
+                if (user) {
+            try {
+                const token = await getToken();
+                await axios.post('/api/cart/update', { cartData }, { headers: { Authorization: `Bearer ${token}` } });
+                toast.success("Item quantity updated in cart");
+
+            } catch (error) {
+                toast.error(error.message);
+            }
+        }
     }
 
     const getCartCount = () => {
@@ -81,11 +135,13 @@ export const AppContextProvider = (props) => {
     }, [])
 
     useEffect(() => {
-        fetchUserData()
-    }, [])
+        if (user){
+            fetchUserData()
+        }
+    }, [user])
 
     const value = {
-        user,
+        user, getToken,
         currency, router,
         isSeller, setIsSeller,
         userData, fetchUserData,
